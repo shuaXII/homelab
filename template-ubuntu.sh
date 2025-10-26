064f5e5c499843f48233a7d61509b1813499faa2
shua <131731528+shuaXII@users.noreply.github.com>
2025-10-26 15:27:46 +0800

    Update template-ubuntu.sh

--- template-ubuntu.sh
+++ template-ubuntu.sh
@@ -9,17 +9,25 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 echo "Adding Docker GPG key and repository..."
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
 echo "Updating and installing dependencies..."
 apt-get update -y -qq
-apt-get install -y -qq curl openssh-client git jq ca-certificates qemu-guest-agent docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y -qq openssh-client git jq qemu-guest-agent docker-ce
 
 echo "Dependencies installed."
 
-echo "Installing and configuring QEMU Guest Agent..."
+echo "Configuring QEMU Guest Agent..."
 systemctl start qemu-guest-agent
 ln -sf /usr/lib/systemd/system/qemu-guest-agent.service /etc/systemd/system/multi-user.target.wants/qemu-guest-agent.service
 
@@ -28,10 +36,10 @@ UBUNTU_CODENAME=${UBUNTU_CODENAME:-$(. /etc/os-release && echo "$VERSION_CODENAM
 echo "deb [arch=$ARCH signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $UBUNTU_CODENAME stable" \
     | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-echo "Updating repositories and upgrading packages..." 
+echo "Upgrading packages..." 
 apt-get upgrade -y -qq
 
-echo "Deploying Docker and Portainer..."
+echo "Deploying Portainer..."
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
 
